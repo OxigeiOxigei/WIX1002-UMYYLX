c9091cc54dbe17de53b26f0957e0371ce7c79167
OxigeiOxigei <71132388+OxigeiOxigei@users.noreply.github.com>
2023-12-06 13:54:01 +0800

    Add files via upload

---
+++ Project7.java
@@ -0,0 +1,13 @@
+package Project7;
+/*
+ * This is our project, we only commit the correct code to here
+ * Therefore, do not delete other members work, we may need to do the debug
+ * or some testing to the code on our own computer.
+ * Anyway, I just don't want that our work disappear in a night
+ * OK, we could start it now
+ */
+public class Project7{
+    public static void main(String[] args){
+        
+    }
+}
